98c20cc1a8f585c84cad1ed85eba846e72e170b5
Codie Aragon <joshiiwoshi@gmail.com>
2022-09-01 22:16:35 -0700

    Added components and css

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+      <h1>
+        HERO
+      </h1>
+    </div>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div id="navbar">
+      <div id="navbar-container">
+      <i className="fa-solid fa-heart"></i>
+        <h1>Codie's Portfolio</h1>
+      </div>
+    </div>
+  )
+}
